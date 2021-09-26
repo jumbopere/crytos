a40062e0cf53c5d53338d0479a188d3c7eee4dca
jumbo pere <jumboperebara0@gmail.com>
2021-09-26 22:05:09 +0100

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         <div className="nav-container">
         <div className="logo-container">
           <Avatar src={icon} size="large" />
-          <Typography.Title level={2} className="logo"><Link to="/">Cryptos</Link></Typography.Title>
+          <Typography.Title level={2} className="logo"><Link to="/">$ryptos</Link></Typography.Title>
           <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
         </div>
         {activeMenu && (
